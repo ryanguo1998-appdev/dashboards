53743b5febbcba880099feba777d4990a0de8dbd
ryanguo1998 <rguo98@uchicago.edu>
2021-07-15 21:39:25 +0000

    Link from forex to next link working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  def all_currencies
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    symbol_hash = parsed_data.fetch("symbols")
+
+    @currency_codes = []
+
+    symbol_hash.each do |key, value|
+      @currency_codes.push(key)
+    end
+
+    render("conversion_templates/currency_pairs.html.erb")
+  end
+
+  def homepage
+    render("conversion_templates/dashboards.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "homepage" })
+  get("/forex", { :controller => "application", :action => "all_currencies" })
+  get("/forex/:first_currency", { :controller => "application", :action => "all_currencies" })
+  get("/forex/:first_currency/:second_currency", { :controller => "application", :action => "all_currencies" })
+  # get(/ , { :controller => "application", :action => "nil"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
