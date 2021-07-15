c1cd79e23fc45c475e48187cbccf0a2d48d4442a
ryanguo1998 <rguo98@uchicago.edu>
2021-07-15 22:49:43 +0000

    Conversion all done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def all_currencies
+  def homepage
+    render("conversion_templates/dashboards.html.erb")
+  end
+
+  def first_currency
     url = "https://api.exchangerate.host/symbols"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
@@ -14,7 +18,32 @@ class ApplicationController < ActionController::Base
     render("conversion_templates/currency_pairs.html.erb")
   end
 
-  def homepage
-    render("conversion_templates/dashboards.html.erb")
+  def second_currency
+    url = "https://api.exchangerate.host/symbols"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    symbol_hash = parsed_data.fetch("symbols")
+
+    @currency_codes = []
+
+    symbol_hash.each do |key, value|
+      @currency_codes.push(key)
+    end
+
+    @first_currency = params.fetch(:pair_one)
+
+    render("conversion_templates/convert.html.erb")
+  end
+  
+  def exchange
+    @first_currency = params.fetch(:pair_one)
+    @second_currency = params.fetch(:pair_two)
+
+    url = "https://api.exchangerate.host/convert?from=" + @first_currency + "&to=" + @second_currency
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @conversion_rate = parsed_data.fetch("info").fetch("rate")
+
+    render("conversion_templates/convert_to.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
-  get("/forex", { :controller => "application", :action => "all_currencies" })
-  get("/forex/:first_currency", { :controller => "application", :action => "all_currencies" })
-  get("/forex/:first_currency/:second_currency", { :controller => "application", :action => "all_currencies" })
+  get("/forex", { :controller => "application", :action => "first_currency" })
+  get("/forex/:pair_one", { :controller => "application", :action => "second_currency" })
+  get("/forex/:pair_one/:pair_two", { :controller => "application", :action => "exchange" })
   # get(/ , { :controller => "application", :action => "nil"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
